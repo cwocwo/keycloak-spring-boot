8b6423c23cfd4318ab4e99cabde9a5eb9ece9cef
caiweiwei <caiweiwei@inspur.com>
2018-01-18 08:56:50 +0800

    init

---
+++ src/main/java/com/inspur/cloud/kkdemo/KkdemoApplication.java
@@ -0,0 +1,12 @@
+package com.inspur.cloud.kkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/inspur/cloud/kkdemo/KkdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inspur.cloud.kkdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KkdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
